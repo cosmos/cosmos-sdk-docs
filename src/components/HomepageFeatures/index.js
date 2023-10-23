1645150c214adf647b88c0b044430c46f69ea83e
Facundo Medica <14063057+facundomedica@users.noreply.github.com>
2023-10-23 21:42:10 +0200

    Add link to title (#73)

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -6,23 +6,23 @@ import Link from "@docusaurus/Link";
 
 function Feature({ Svg, to, title, description }) {
   return (
-    <div className={clsx("col col--4")}>
-      <Link to={to}>
+    <Link to={to}>
+      <div className={clsx("col col--4")}>
         <div className=" text--center">
           <Svg className={styles.featureSvg} role="img" />
         </div>
-      </Link>
-      <div className="text--center padding-horiz--md">
-        <h3 className="py-5">
-          <strong>{title}</strong>
-        </h3>
-        <p className="pb-10">{description}</p>
+        <div className="text--center padding-horiz--md">
+          <h3 className="py-5">
+            <strong>{title}</strong>
+          </h3>
+          <p className="pb-10">{description}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
-export default function HomepageFeatures({list}) {
+export default function HomepageFeatures({ list }) {
   return (
     <section className={styles.features}>
       <div className="container pt-9">
